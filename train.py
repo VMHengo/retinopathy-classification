bef6e44bf0028d4ff19d09e12a0a329b215da699
VMHNguyen <hoang.nguyen@rwth-aachen.de>
2026-06-30 15:08:28 +0200

    implemented seperate run_epoch function that returns loss and acc for given dataset

--- train.py
+++ train.py
@@ -20,78 +20,91 @@ def load_info(data_flag:str) -> (str, int, int):
     n_classes = 2 # reduce classes from 0-4 (low to high) to 0-1 (neglible or severe)
     return task, n_channels, n_classes
 
+def run_epoch(dataloader, model, criterion, device, optimizer=None):
+    training_active = optimizer is not None
+
+    total_correct = 0
+    total_loss = 0.0
+    average_loss = 0
+    average_acc = 0
+    total_inputs = 0
+    
+    model.train()
+    for inputs, targets in tqdm(dataloader):
+
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        outputs = model(inputs)
+        
+        targets = targets.to(torch.float32).unsqueeze(1)
+        loss = criterion(outputs, targets)
+        preds = (outputs >= 0).long() # interpret logit values as binary predictions
+        correct_pred_vect = (preds == targets) # tensor([[bool], [bool], ...])
+        total_correct += correct_pred_vect.sum().item() # sum of correct vectors as tesnor -> extract int with .item()
+        
+        total_inputs += inputs.shape[0]
+        batch_size = inputs.shape[0]
+        total_loss += loss.item() * batch_size
+
+        if training_active:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    
+    average_loss = total_loss / total_inputs
+    average_acc = total_correct / total_inputs
+    
+    return average_loss, average_acc
 
 def train():
-    NUM_EPOCHS = 3
+    NUM_EPOCHS = 10
     BATCH_SIZE = 128
-    lr = 0.001
+    lr = 0.001 #learning rate
 
     task = "multi-label, binary-class"
     device = torch.device('cpu')
     train_dataloader, test_dataloader, val_dataloader = get_dataloaders('retinamnist')
     model = ResNet18Binary().to(device)
-    criterion = nn.BCEWithLogitsLoss() if task == "multi-label, binary-class" else nn.CrossEntropyLoss()
-    optimizers = ( optim.Adam(model.parameters(), lr=lr), 
-                   optim.SGD(model.parameters(), lr=lr, momentum=0.9) )
+    criterion = nn.BCEWithLogitsLoss()
+    optimizers = ( optim.Adam(model.parameters(), lr=lr), # Adaptive Moment Estimation
+                   optim.SGD(model.parameters(), lr=lr, momentum=0.9) ) # Stochastic Gradient Descent
 
     for optimizer in optimizers:
         for epoch in range(NUM_EPOCHS):
-            train_correct = 0
-            train_total = 0
-            test_correct = 0
-            test_total = 0
+            train_loss, train_acc = run_epoch(
+                dataloader=train_dataloader,
+                model=model,
+                criterion=criterion,
+                device=device,
+                optimizer=optimizer
+            )
+            val_loss, val_acc = run_epoch(
+                dataloader=val_dataloader,
+                model=model,
+                criterion=criterion,
+                device=device
+            )
             
-            model.train()
-            for inputs, targets in tqdm(train_dataloader):
-                # forward + backward + optimize
-                optimizer.zero_grad()
-                outputs = model(inputs)
-                
-                if task == 'multi-label, binary-class':
-                    targets = targets.to(torch.float32).unsqueeze(1)
-                    loss = criterion(outputs, targets)
-                else:
-                    targets = targets.squeeze().long()
-                    loss = criterion(outputs, targets)
-                
-                loss.backward()
-                optimizer.step()
+            # print(f"Epoch {epoch+1}/{NUM_EPOCHS}")
+            # print(f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}")
+            # print(f"Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}")
 
+        # run on test set to see final performance
+        test_loss, test_acc = run_epoch(
+            dataloader=test_dataloader,
+            model=model,
+            criterion=criterion,
+            device=device
+        )
 
-# evaluation
+        print("-------------------------------------------------------------")
+        print(f"Optimizer: {optimizer.__class__.__name__}")
+        print(f"Final Train Loss: {train_loss:.4f}, Final Train Acc: {train_acc:.4f}")
+        print(f"Final Val Loss: {val_loss:.4f}, Final Val Acc: {val_acc:.4f}")
+        print(f"Final Test Loss: {test_loss:.4f}, Final Test Acc: {test_acc:.4f}")
+        print("-------------------------------------------------------------")
+        print("\n")
 
-def test(split):
-    model.eval()
-    y_true = torch.tensor([])
-    y_score = torch.tensor([])
-    
-    data_loader = train_loader_at_eval if split == 'train' else test_loader
-
-    with torch.no_grad():
-        for inputs, targets in data_loader:
-            outputs = model(inputs)
-
-            if task == 'multi-label, binary-class':
-                targets = targets.to(torch.float32)
-                outputs = outputs.softmax(dim=-1)
-            else:
-                targets = targets.squeeze().long()
-                outputs = outputs.softmax(dim=-1)
-                targets = targets.float().resize_(len(targets), 1)
-
-            y_true = torch.cat((y_true, targets), 0)
-            y_score = torch.cat((y_score, outputs), 0)
-
-        y_true = y_true.numpy()
-        y_score = y_score.detach().numpy()
-        
-        evaluator = Evaluator(data_flag, split)
-        metrics = evaluator.evaluate(y_score)
-    
-        print('%s  auc: %.3f  acc:%.3f' % (split, *metrics))
 
 if __name__ == "__main__":
     train()
-    print('==> Evaluating ...')
-    test('train')
-    test('test')
